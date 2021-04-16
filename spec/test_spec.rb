e1187e82fc0ef4afe68070744c8316fd1d63f8b5
EC2 Default User <ec2-user@ip-172-31-26-173.ec2.internal>
2021-04-16 02:52:42 +0000

    Rspecテスト練習

---
+++ spec/test_spec.rb
@@ -0,0 +1,19 @@
+describe '四則演算' do
+  context '足し算' do
+    it '1+1は2になる' do
+      expect(1 + 1).to eq 2
+    end
+  end
+  
+  context '足し算' do
+    it '1+1は2になる' do
+      expect(1 + 1).to eq 3
+    end
+  end
+  
+  context '引き算' do
+    it '3-2は１になる' do
+      expect(3 - 2).to eq 1
+    end
+  end
+end
